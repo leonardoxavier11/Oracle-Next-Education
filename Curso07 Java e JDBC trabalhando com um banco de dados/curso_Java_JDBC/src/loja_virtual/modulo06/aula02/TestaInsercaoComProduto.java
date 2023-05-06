b38c399a1d64d98e9e4235c9a0bbf55da181a0bd
leonardoxavier11 <104578415+leonardoxavier11@users.noreply.github.com>
2023-05-06 14:50:47 -0300

    Nova Branch

--- Curso07 Java e JDBC trabalhando com um banco de dados/curso_Java_JDBC/src/loja_virtual/modulo06/aula02/TestaInsercaoComProduto.java
+++ Curso07 Java e JDBC trabalhando com um banco de dados/curso_Java_JDBC/src/loja_virtual/modulo06/aula02/TestaInsercaoComProduto.java
@@ -1,10 +1,6 @@
 package loja_virtual.modulo06.aula02;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import loja_virtual.modulo06.aula02.modelo.Produto;
 
@@ -12,28 +8,30 @@ public class TestaInsercaoComProduto {
 
 	public static void main(String[] args) throws SQLException {
 
-		Produto comada = new Produto("Comoda", "Comoda Vertical");
+		Produto produto = new Produto("janela", "janela de aluminio");
 
-		try (Connection con = new ConnectionFactory().recuperarConexao()) {
+//		try (Connection con = new ConnectionFactory().recuperarConexao()) {
+//
+//			String sql = "INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?, ?)";
+//
+//			try (PreparedStatement pstm = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+//
+//				pstm.setString(1, comada.getNome());
+//				pstm.setString(2, comada.getDescricao());
+//
+//				pstm.execute();
+//
+//				try (ResultSet rst = pstm.getGeneratedKeys()) {
+//					while (rst.next()) {
+//						comada.setId(rst.getInt(1));
+//					}
+//				}
+//			}
+//
+//		}
 
-			String sql = "INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?, ?)";
-
-			try (PreparedStatement pstm = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-
-				pstm.setString(1, comada.getNome());
-				pstm.setString(2, comada.getDescricao());
-
-				pstm.execute();
-
-				try (ResultSet rst = pstm.getGeneratedKeys()) {
-					while (rst.next()) {
-						comada.setId(rst.getInt(1));
-					}
-				}
-			}
-
-		}
-		System.out.println(comada);
+		produto.setProduto();
+		System.out.println(produto);
 
 	}
 }
--- Curso07 Java e JDBC trabalhando com um banco de dados/curso_Java_JDBC/src/loja_virtual/modulo06/aula02/modelo/Produto.java
+++ Curso07 Java e JDBC trabalhando com um banco de dados/curso_Java_JDBC/src/loja_virtual/modulo06/aula02/modelo/Produto.java
@@ -1,5 +1,13 @@
 package loja_virtual.modulo06.aula02.modelo;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import loja_virtual.modulo06.aula02.ConnectionFactory;
+
 public class Produto {
 
 	private Integer id;
@@ -28,4 +36,27 @@ public class Produto {
 	public String toString() {
 		return String.format("O produto é: %d, %s, %s", this.id, this.nome, this.descricao);
 	}
+
+	public void setProduto() throws SQLException {
+		try (Connection con = new ConnectionFactory().recuperarConexao()) {
+
+			String sql = "INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?, ?)";
+
+			try (PreparedStatement pstm = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+				pstm.setString(1, this.getNome());
+				pstm.setString(2, this.getDescricao());
+
+				pstm.execute();
+
+				try (ResultSet rst = pstm.getGeneratedKeys()) {
+					while (rst.next()) {
+						this.setId(rst.getInt(1));
+					}
+				}
+			}
+
+		}
+	}
+
 }
