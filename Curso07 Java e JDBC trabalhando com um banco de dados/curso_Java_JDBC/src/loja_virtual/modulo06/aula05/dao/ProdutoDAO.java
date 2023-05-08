0577b05c316653d9638ae8b628726b6fcec285e7
leonardoxavier11 <104578415+leonardoxavier11@users.noreply.github.com>
2023-05-08 11:38:43 -0300

    Criando os package MODELO e DAO

---
+++ Curso07 Java e JDBC trabalhando com um banco de dados/curso_Java_JDBC/src/loja_virtual/modulo06/aula05/dao/ProdutoDAO.java
@@ -0,0 +1,38 @@
+package loja_virtual.modulo06.aula05.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import loja_virtual.modulo06.aula03.modelo.Produto;
+
+public class ProdutoDAO {
+
+	private Connection con;
+
+	public ProdutoDAO(Connection con) {
+		this.con = con;
+	}
+
+	public void salvar(Produto produto) throws SQLException {
+
+		String sql = "INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?, ?)";
+
+		try (PreparedStatement pstm = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+			pstm.setString(1, produto.getNome());
+			pstm.setString(2, produto.getDescricao());
+
+			pstm.execute();
+
+			try (ResultSet rst = pstm.getGeneratedKeys()) {
+				while (rst.next()) {
+					produto.setId(rst.getInt(1));
+				}
+			}
+		}
+
+	}
+}
